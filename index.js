216b371ca11a7487ee4513786d1cb34daad10c95
romeo-art <romeo.lenizo@student.passerellesnumeriques.org>
2019-09-24 17:21:11 +0800

    iniksla;fksd

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-
+var port = process.env.PORT ||3000
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+console.log('listening on *:' + port);
+
 });
